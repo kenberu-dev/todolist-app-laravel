0ddd0f7db72bc8968c4b5f5fd9ee530f86d4d6b9
kenberu-dev <kenberu.development@gmail.com>
2024-04-02 15:49:01 +0900

    コンポーネントを作成

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Todoアプリ' }}</title>
-    <link href="/css/app.css" rel="stylesheet">
+    <!-- <link href="/css/app.css" rel="stylesheet"> -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50">
     {{ $slot }}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,13 @@
 <x-layout title="TOP | Todoアプリ">
-    <h1>ここに内容が入ります</h1>
+    <x-layout.single>
+        <h1 class="text-center text-green-500 text-4xl font-bold mt-8 mb-8">
+            Todoアプリ
+        </h1>
+        <div class="flex flex-wrap justify-center">
+            <div class="w-1/2 p-4 flex flex-wrap justify-evenly">
+                <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
+                <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
+            </div>
+        </div>
+    </x-layout.single>
 </x-layout>
